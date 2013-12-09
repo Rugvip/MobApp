82523902da1e5af27d20250ff6932dd6df448c9d
Brickhead <axel.odelberg@gmail.com>
2013-12-09 18:36:42 +0100

    Sensorsimulator for android emulator.

--- Lab3Project/Lab3/src/main/java/se/kth/oberg/lab3/MainActivity.java
+++ Lab3Project/Lab3/src/main/java/se/kth/oberg/lab3/MainActivity.java
@@ -1,24 +1,46 @@
 package se.kth.oberg.lab3;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
+import android.app.Activity;
 import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.os.StrictMode;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
+import android.widget.TextView;
 
-public class MainActivity extends ActionBarActivity {
+import org.openintents.sensorsimulator.hardware.Sensor;
+import org.openintents.sensorsimulator.hardware.SensorEvent;
+import org.openintents.sensorsimulator.hardware.SensorEventListener;
+import org.openintents.sensorsimulator.hardware.SensorManagerSimulator;
+
+public class MainActivity extends Activity implements SensorEventListener {
+
+    private TextView dataOutput;
+    private SensorManagerSimulator mSensorManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        dataOutput = (TextView) findViewById(R.id.textview_output);
+
+        mSensorManager = SensorManagerSimulator.getSystemService(this, SENSOR_SERVICE);
+        mSensorManager.connectSimulator();
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManagerSimulator.SENSOR_DELAY_UI);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mSensorManager.unregisterListener(this);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -33,5 +55,15 @@ public class MainActivity extends ActionBarActivity {
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+            Log.e("onSensorChanged", "X " + event.values[0] + "Y: " + event.values[1]);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
     }
 }
