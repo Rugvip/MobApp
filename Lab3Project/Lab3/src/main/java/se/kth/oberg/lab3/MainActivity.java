fe7500a47d48a643ac09b867953e4bd2b1fa8d64
Brickhead <axel.odelberg@gmail.com>
2013-12-10 19:13:23 +0100

    Removed simulator, new normalmode

--- Lab3Project/Lab3/src/main/java/se/kth/oberg/lab3/MainActivity.java
+++ Lab3Project/Lab3/src/main/java/se/kth/oberg/lab3/MainActivity.java
@@ -1,39 +1,36 @@
 package se.kth.oberg.lab3;
 
 import android.app.Activity;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
-import android.os.StrictMode;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 
-import org.openintents.sensorsimulator.hardware.Sensor;
-import org.openintents.sensorsimulator.hardware.SensorEvent;
-import org.openintents.sensorsimulator.hardware.SensorEventListener;
-import org.openintents.sensorsimulator.hardware.SensorManagerSimulator;
-
 public class MainActivity extends Activity implements SensorEventListener {
 
     private TextView dataOutput;
-    private SensorManagerSimulator mSensorManager;
+    private SensorManager mSensorManager;
+    private Sensor mSensor;
+    private float[] samples = new float[]{0.0f,0.0f,0.0f};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-        StrictMode.setThreadPolicy(policy);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         dataOutput = (TextView) findViewById(R.id.textview_output);
 
-        mSensorManager = SensorManagerSimulator.getSystemService(this, SENSOR_SERVICE);
-        mSensorManager.connectSimulator();
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManagerSimulator.SENSOR_DELAY_UI);
+        mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_UI);
     }
 
     @Override
@@ -58,12 +55,21 @@ public class MainActivity extends Activity implements SensorEventListener {
     }
 
     @Override
-    public void onSensorChanged(SensorEvent event) {
-            Log.e("onSensorChanged", "X " + event.values[0] + "Y: " + event.values[1]);
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
     }
 
     @Override
-    public void onAccuracyChanged(Sensor sensor, int i) {
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        final float alpha = 0.8f;
+        samples[0] = alpha * samples[0] + (1-alpha) * sensorEvent.values[0];
+        samples[1] = alpha * samples[1] + (1-alpha) * sensorEvent.values[1];
+        samples[2] = alpha * samples[2] + (1-alpha) * sensorEvent.values[2];
 
+        Log.e("onSensorChanged", "Values: "
+                + samples[0]
+                + " 1: " + samples[1]
+                + " 2: " + samples[2]
+        );
     }
 }
