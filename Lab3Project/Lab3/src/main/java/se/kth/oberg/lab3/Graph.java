a3d76c1db6d49327aab39e1105d3a92d88e04e4e
Brickhead <axel.odelberg@gmail.com>
2013-12-12 19:46:59 +0100

    added 2 more graphs, y and Z

--- Lab3Project/Lab3/src/main/java/se/kth/oberg/lab3/Graph.java
+++ Lab3Project/Lab3/src/main/java/se/kth/oberg/lab3/Graph.java
@@ -3,28 +3,35 @@ package se.kth.oberg.lab3;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
-import android.hardware.SensorManager;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Graph extends SurfaceView implements GraphAccelerationListener {
-    private SensorManager mSensorManager;
     private SurfaceHolder holder = getHolder();
-    private static final int BUFF_SIZE = 100;
-    private float[] graphData = new float[BUFF_SIZE];
+    private static final int BUFF_SIZE = 500;
+    private float[][] graphData = new float[3][BUFF_SIZE];
     private int dataStart = 0;
     private int dataEnd = 0;
 
     private Paint X_GRAPH = new Paint();
-    private static final float RECT_SIZE = 50f;
+    private Paint Y_GRAPH = new Paint();
+    private Paint Z_GRAPH = new Paint();
 
-    private float accelerationX, accelerationY, acecelerationZ;
+    List<Paint> paints = new ArrayList<Paint>();
 
     public Graph(Context context, AttributeSet attrs) {
         super(context, attrs);
         X_GRAPH.setColor(0xFF0000FF);
+        Y_GRAPH.setColor(0xFFFF0000);
+        Z_GRAPH.setColor(0xFF00FF00);
+        paints.add(X_GRAPH);
+        paints.add(Y_GRAPH);
+        paints.add(Z_GRAPH);
     }
 
     @Override
@@ -37,28 +44,31 @@ public class Graph extends SurfaceView implements GraphAccelerationListener {
 
         canvas.save();
         Log.e("ased", "end: " + dataEnd + "start: " + dataStart);
-        int offsetX = 0;
-        for (int i = dataEnd; i != dataStart; i--) {
-            if (i < 1) {
-                canvas.drawLine(-widthPerBuffs * offsetX, graphData[i], -widthPerBuffs * (offsetX + 1), graphData[BUFF_SIZE - 1], X_GRAPH);
-                i = BUFF_SIZE - 1;
-            } else {
-                canvas.drawLine(-widthPerBuffs * offsetX, graphData[i], -widthPerBuffs * (offsetX + 1), graphData[i - 1], X_GRAPH);
+        for (int j = 0; j< paints.size();j++) {
+            int offsetX = 0;
+            for (int i = dataEnd; i != dataStart; i--) {
+                if (i < 1) {
+                    canvas.drawLine(-widthPerBuffs * offsetX, graphData[j][i], -widthPerBuffs * (offsetX + 1), graphData[j][BUFF_SIZE - 1], paints.get(j));
+                    i = BUFF_SIZE;
+                } else {
+                    canvas.drawLine(-widthPerBuffs * offsetX, graphData[j][i], -widthPerBuffs * (offsetX + 1), graphData[j][i - 1], paints.get(j));
+                }
+                offsetX++;
             }
-            offsetX++;
         }
         canvas.restore();
     }
 
     @Override
     public void accelerationHappend(float x, float y, float z) {
-        graphData[dataEnd] = x;
-        dataEnd = (dataEnd + 1) % graphData.length;
+        graphData[0][dataEnd] = x;
+        graphData[1][dataEnd] = y;
+        graphData[2][dataEnd] = z;
+        dataEnd = (dataEnd + 1) % graphData[0].length;
         if (dataEnd == dataStart) {
-            dataStart = (dataStart + 1) % graphData.length;
+            dataStart = (dataStart + 1) % graphData[0].length;
         }
 
-        this.accelerationX = x;
         Canvas canvas = holder.lockCanvas();
         if (canvas != null) {
             draw(canvas);
