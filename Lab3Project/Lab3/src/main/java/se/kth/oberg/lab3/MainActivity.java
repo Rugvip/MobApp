874588083eafccd681364e84f468f5df8f99de68
Brickhead <axel.odelberg@gmail.com>
2013-12-12 02:23:13 +0100

    test: Open the GL

--- Lab3Project/Lab3/src/main/java/se/kth/oberg/lab3/MainActivity.java
+++ Lab3Project/Lab3/src/main/java/se/kth/oberg/lab3/MainActivity.java
@@ -1,6 +1,8 @@
 package se.kth.oberg.lab3;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.hardware.Sensor;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.Menu;
@@ -11,12 +13,12 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends Activity implements FlowerFlexListener, FlowerShakeListener {
+public class MainActivity extends Activity implements FlowerFlexListener, FlowerShrivelListener {
 
     private FlowerFlexSensor ffs = new FlowerFlexSensor(this);
-    private FlowerShakeSensor fss = new FlowerShakeSensor(this);
+    private FlowerShrivelSensor fss = new FlowerShrivelSensor(this);
+    private TextView dataOutput;
     private SensorManager mSensorManager;
-    private Flower flower;
 
     private ProgressBar progressBar;
 
@@ -24,8 +26,8 @@ public class MainActivity extends Activity implements FlowerFlexListener, Flower
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        dataOutput = (TextView) findViewById(R.id.textview_output);
         progressBar = (ProgressBar) findViewById(R.id.progressBar);
-        flower = (Flower) findViewById(R.id.flower);
 
         mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
     }
@@ -34,7 +36,7 @@ public class MainActivity extends Activity implements FlowerFlexListener, Flower
     protected void onResume() {
         super.onResume();
         mSensorManager.registerListener(ffs, mSensorManager.getDefaultSensor(FlowerFlexSensor.SENSOR_TYPE), SensorManager.SENSOR_DELAY_GAME);
-        mSensorManager.registerListener(fss, mSensorManager.getDefaultSensor(FlowerShakeSensor.SENSOR_TYPE), SensorManager.SENSOR_DELAY_GAME);
+        mSensorManager.registerListener(fss, mSensorManager.getDefaultSensor(FlowerShrivelSensor.SENSOR_TYPE), SensorManager.SENSOR_DELAY_GAME);
     }
 
     @Override
@@ -61,13 +63,11 @@ public class MainActivity extends Activity implements FlowerFlexListener, Flower
 
     @Override
     public void onFlex(double angle) {
-        flower.flex(angle);
         progressBar.setProgress((int) angle + 90);
     }
 
     @Override
-    public void onShake() {
-        flower.exterminate();
+    public void onShrivel() {
         Toast.makeText(this,"Shaked!",Toast.LENGTH_SHORT).show();
     }
 }
